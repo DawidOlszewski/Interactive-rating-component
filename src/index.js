9077e2dac257a55191fc2e22ba659f913e99d6e0
Dawid Olszewski <dawid.olszewski.dev@gmail.com>
2022-06-03 00:21:16 +0200

    add transitions btw sections after clicking submit btn

--- src/index.js
+++ src/index.js
@@ -1,19 +1,41 @@
 const ratingLabels = document.querySelectorAll("label")
+const submitBtn  = document.querySelector("input[type='button']")
+const sections = document.querySelectorAll("section")
+const ratingInfo = document.querySelector(".rating-value-informator")
+
+changeShownRating(1)
 
 ratingLabels.forEach(ratingLabel => 
     ratingLabel.addEventListener("click", updateRadio)
-    
 );
+
+submitBtn.addEventListener("click", displayAcknowledgement)
+
 
 
 function updateRadio(){
     ratingLabels.forEach((ratingLable,index)=>{
-        ratingLable.classList.remove("checked")
+        ratingLable.classList.value = "rating-label"
         var rating = ratingLable.querySelector("input")
 
         if(rating.checked){
-            console.log(index)
             ratingLable.classList.add("checked")
+            changeShownRating(index+1)
         }
 })
+}
+
+function displayAcknowledgement(){
+    sectionsHiddingToggle()
+    setTimeout(sectionsHiddingToggle, 2000)
+}
+
+function sectionsHiddingToggle(){
+    sections[0].classList.toggle('hidden')
+    sections[1].classList.toggle('hidden')
+}
+
+function changeShownRating(new_rating){
+    const template = `You seleted ${new_rating} out of 5`
+    ratingInfo.innerText = template;
 }
